5f0f8cafe4be974b097fbefe1bddb12df9024552
Leo Booth <leoboothtx@gmail.com>
2025-03-30 01:59:07 -0500

    updated WaitFluent to reuse code, added support for findElements()

--- src/main/java/com/leobooth/practice/framework/waits/WaitFluent.java
+++ src/main/java/com/leobooth/practice/framework/waits/WaitFluent.java
@@ -1,13 +1,11 @@
 package com.leobooth.practice.framework.waits;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.ElementNotInteractableException;
-import org.openqa.selenium.TimeoutException;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
 
 import java.time.Duration;
+import java.util.ArrayList;
 
 public class WaitFluent {
 
@@ -33,32 +31,67 @@ public class WaitFluent {
         return fluentWait(driver, timeout, polling);
     }
 
-    public static void untilElementVisible(WebDriver driver, By byPageObjectLocator) {
-        try {
-            Wait<WebDriver> wait = forSeconds(driver, defaultMaxWaitTimeInSeconds);
-            wait.until( d -> driver.findElement(byPageObjectLocator).isDisplayed());
-        } catch (TimeoutException te) {
-            throw new TimeoutException("Element did not appear before maximum wait time of "
-                                        + defaultMaxWaitTimeInSeconds + " seconds was exceeded.", te);
-        }
+    public static void untilElementIsDisplayed(WebDriver driver, By byLocator)
+            throws TimeoutException {
+        untilElementIsDisplayed(driver, defaultMaxWaitTimeInSeconds, defaultMillisBeforeRetry, byLocator);
     }
 
-    public static void untilElementVisible(WebDriver driver, int maxWaitTimeInSeconds, By byPageObjectLocator) {
+    public static void untilElementIsDisplayed(
+            WebDriver driver, int maxWaitTimeInSeconds, By byLocator
+            ) throws TimeoutException {
+        untilElementIsDisplayed(driver, maxWaitTimeInSeconds, defaultMillisBeforeRetry, byLocator);
+    }
+
+    public static void untilElementIsDisplayed(
+            WebDriver driver, int maxWaitTimeInSeconds, int millisBeforeRetry, By byLocator)
+            throws TimeoutException {
         try {
-            Wait<WebDriver> wait = forSeconds(driver, maxWaitTimeInSeconds);
-            wait.until( d -> driver.findElement(byPageObjectLocator).isDisplayed());
+            Wait<WebDriver> wait = customWait(driver, maxWaitTimeInSeconds, millisBeforeRetry);
+            wait.until( d -> {
+                try {
+                    driver.findElement(byLocator).isDisplayed();
+                    return true;
+                } catch (NoSuchElementException e) {
+                    return false;
+                }
+            });
         } catch (TimeoutException te) {
             throw new TimeoutException("Element did not appear before maximum wait time of "
                     + maxWaitTimeInSeconds + " seconds was exceeded.", te);
         }
     }
 
-    public static void untilElementVisible(WebDriver driver, int maxWaitTimeInSeconds, int millisBeforeRetry, By byPageObjectLocator) {
+    public static void untilElementsAreDisplayed(WebDriver driver, By byLocator)
+            throws TimeoutException {
+        untilElementsAreDisplayed(driver, defaultMaxWaitTimeInSeconds, defaultMillisBeforeRetry, byLocator);
+    }
+
+    public static void untilElementsAreDisplayed(
+            WebDriver driver, int maxWaitTimeInSeconds, By byLocator
+    ) throws TimeoutException {
+        untilElementsAreDisplayed(driver, maxWaitTimeInSeconds, defaultMillisBeforeRetry, byLocator);
+    }
+
+    public static void untilElementsAreDisplayed(
+            WebDriver driver, int maxWaitTimeInSeconds, int millisBeforeRetry, By byLocator)
+            throws TimeoutException {
         try {
             Wait<WebDriver> wait = customWait(driver, maxWaitTimeInSeconds, millisBeforeRetry);
-            wait.until( d -> driver.findElement(byPageObjectLocator).isDisplayed());
+            wait.until( d -> {
+                ArrayList<WebElement> elements = new ArrayList<>(driver.findElements(byLocator));
+                if (elements.isEmpty()) {
+                    throw new NoSuchElementException("No elements were found matching the locator provided.");
+                }
+                for (WebElement element : elements) {
+                    if (!element.isDisplayed()){
+                        return false;
+                    };
+                }
+
+                return true;
+            });
         } catch (TimeoutException te) {
-            throw new TimeoutException("Element did not appear before maximum wait time of "
+            throw new TimeoutException("Elements did not appear before maximum wait time of "
                     + maxWaitTimeInSeconds + " seconds was exceeded.", te);
         }
     }
