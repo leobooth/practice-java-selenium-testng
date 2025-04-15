97d2e85020d228d8b227e8996e1d2938d72d197b
Leo Booth <leoboothtx@gmail.com>
2025-04-14 21:10:52 -0500

    added 'testBrowser' property; can select browser via command line or within test

--- src/main/java/com/leobooth/practice/framework/baseObjects/BaseTest.java
+++ src/main/java/com/leobooth/practice/framework/baseObjects/BaseTest.java
@@ -6,10 +6,7 @@ import java.util.Map;
 
 import com.leobooth.practice.framework.waits.WaitFluent;
 import io.github.cdimascio.dotenv.Dotenv;
-import org.openqa.selenium.By;
-import org.openqa.selenium.TimeoutException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterClass;
@@ -17,60 +14,84 @@ import org.testng.asserts.SoftAssert;
 
 public class BaseTest {
 
-  public static Dotenv ENV_VARS = Dotenv.configure().load();
-  private ArrayList<WebDriver> testDrivers = new ArrayList<>();
+    public static Dotenv ENV_VARS = Dotenv.configure().load();
+    private ArrayList<WebDriver> testDrivers = new ArrayList<>();
 
-  public WebDriver setupTestDriverChrome() {
-    WebDriver driver = new ChromeDriver();
-    testDrivers.add(driver);
-    return driver;
-  }
+    public WebDriver setupTestDriverChrome() {
+        WebDriver driver = new ChromeDriver();
+        testDrivers.add(driver);
+        return driver;
+    }
 
-  public WebDriver setupTestDriverEdge() {
-    WebDriver driver = new EdgeDriver();
-    testDrivers.add(driver);
-    return driver;
-  }
+    public WebDriver setupTestDriverEdge() {
+        WebDriver driver = new EdgeDriver();
+        testDrivers.add(driver);
+        return driver;
+    }
 
-  // TODO: use this as part of a crawler / graph-walking test?
-  public SoftAssert testExpectedLinksPresent(WebDriver driver, LinkedHashMap<String, By> links) {
-    SoftAssert softAssert = new SoftAssert();
-    for (Map.Entry<String, By> link : links.entrySet()) {
-      boolean linkFound = false;
-      try {
-        WaitFluent.untilElementsAreDisplayed(driver, link.getValue());
-        ArrayList<WebElement> elements = new ArrayList<>(driver.findElements(link.getValue()));
-        if (elements.isEmpty()) {
-          linkFound = false;
+    public WebDriver setupTestDriver(String testBrowserRequest) {
+        String testBrowser = "";
+
+        if (testBrowserRequest.isBlank()) {
+            testBrowser = System.getProperty("testBrowser").toUpperCase().trim();
         } else {
-          for (WebElement element : elements) {
-            if (!element.isDisplayed()) {
-              linkFound = false;
-              System.out.println("Did not find link: " + link.getKey());
+            testBrowser = testBrowserRequest.toUpperCase().trim();
+        }
+
+        WebDriver driver = switch(testBrowser) {
+            case "CHROME" -> setupTestDriverChrome();
+            case "EDGE" -> setupTestDriverEdge();
+            default -> throw new InvalidArgumentException(
+                    "Invalid test browser requested: " + testBrowser);
+        };
+
+        return driver;
+    }
+
+    public WebDriver setupTestDriver() {
+        return setupTestDriver("");
+    }
+
+    // TODO: use this later as part of a crawler / graph-walking test?
+    public SoftAssert testExpectedLinksPresent(WebDriver driver, LinkedHashMap<String, By> links) {
+        SoftAssert softAssert = new SoftAssert();
+        for (Map.Entry<String, By> link : links.entrySet()) {
+            boolean linkFound = false;
+            try {
+                WaitFluent.untilElementsAreDisplayed(driver, link.getValue());
+                ArrayList<WebElement> elements = new ArrayList<>(driver.findElements(link.getValue()));
+                if (elements.isEmpty()) {
+                    linkFound = false;
+                } else {
+                    for (WebElement element : elements) {
+                        if (!element.isDisplayed()) {
+                            linkFound = false;
+                            System.out.println("Did not find link: " + link.getKey());
+                        }
+                    }
+                }
+                linkFound = true;
+                System.out.println("Found link: " + link.getKey());
+            } catch (TimeoutException e) {
+                linkFound = false;
+                System.out.println("Did not find link: " + link.getKey());
+            } finally {
+                softAssert.assertTrue(linkFound);
             }
-          }
         }
-        linkFound = true;
-        System.out.println("Found link: " + link.getKey());
-      } catch (TimeoutException e) {
-        linkFound = false;
-        System.out.println("Did not find link: " + link.getKey());
-      } finally {
-        softAssert.assertTrue(linkFound);
-      }
+
+        return softAssert;
     }
 
-    return softAssert;
-  }
-
-  @AfterClass(alwaysRun = true)
-  public void tearDown() {
-    if (!testDrivers.isEmpty()) {
-      for(WebDriver driver : testDrivers) {
-        if (driver != null) {
-          driver.quit();
+    @AfterClass(alwaysRun = true)
+    public void tearDown() {
+        if (!testDrivers.isEmpty()) {
+            for(WebDriver driver : testDrivers) {
+                if (driver != null) {
+                    driver.quit();
+                }
+            }
         }
-      }
     }
-  }
+
 }
